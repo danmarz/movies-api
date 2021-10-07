ab4ae0545e982a0a8e68036daaf2ef7711f2010f
Dan Marius Dumitrescu <dumy.tm@gmail.com>
2021-10-08 01:03:58 +0200

    initial upload

---
+++ src/js/index.js
@@ -0,0 +1,26 @@
+//Global APP controller
+import Search from './models/Search';
+
+const data = {
+
+};
+window.data = data;
+
+const searchController = async () => {
+    //GET SEARCH QUERY FROM THE FORM
+    const query = `Batman`;
+    if(query){
+        //CREATE NEW OBJ FROM SEARCH CLASS
+        data.search = new Search(query);
+
+        //MOVIE SEARCH
+        await data.search.getSearchResult();
+        console.log(data.search.movies);
+    }
+}
+
+document.querySelector('.movie-search').addEventListener('submit', e => {
+    e.preventDefault();
+    //CALL SEARCH CONTROLLER
+    searchController();
+})
