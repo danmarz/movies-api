f54e8395811eeb07b9934c52be473a172737a6c3
Dan Marius Dumitrescu <dumy.tm@gmail.com>
2021-11-12 16:34:22 +0100

    added comments

--- src/js/models/Movie.js
+++ src/js/models/Movie.js
@@ -9,7 +9,7 @@ export default class Movie {
         try {
             const result = await fetch(`${api}i=${this.imdbID}&apikey=${key}`);
             const data = await result.json();
-            // console.log(data);
+            console.log(data);
             this.title = data.Title;
             this.year = data.Year;
             this.rated = data.Rated;
@@ -24,7 +24,7 @@ export default class Movie {
             this.director = data.Director;
             this.imdbVotes = data.imdbVotes;
             this.url = `${movieUrl}${this.imdbID}/`;
-            this.numTickets = 1; 
+            this.numTickets = 1;
             this.price = 10;
             this.getMainActor();
             this.movieCrew(data.Writer);
@@ -33,17 +33,17 @@ export default class Movie {
             alert('Something went wrong loading the movie');
         }
     }
-//GET THE MAIN ACTOR FROM AN ARRAY
+    //GET THE MAIN ACTOR FROM AN ARRAY
     getMainActor() {
         const newActor = this.actors.split(', ');
         this.actors = newActor;
         this.mainActor = newActor[0];
     };
-//CREATING MOVIE CREW
+    //CREATING MOVIE CREW
     movieCrew(writer) {
         this.writer = writer.split(', ');
     };
-//MOVIE RATING
+    //MOVIE RATING
     movieRating(imdbRating) {
         // imdbRating = '10';
         this.ratingArray = [];
@@ -57,20 +57,20 @@ export default class Movie {
             let intPart = parseInt(theArray[0]);
             let decPart = parseInt(theArray[1]);
             if (decPart > 0) {
-                for(let i = 0; i < intPart; i++){
+                for (let i = 0; i < intPart; i++) {
                     this.ratingArray.push(i);
                 }
                 this.ratingArray.push(0.5);
             }
             else {
-                for(let i = 0; i < intPart; i++){
+                for (let i = 0; i < intPart; i++) {
                     this.ratingArray.push(i);
                 }
             }
         }
     };
-//ADD A TICKET OR REMOVE A TICKET
-    updateTickets(ticketType){
+    //ADD A TICKET OR REMOVE A TICKET
+    updateTickets(ticketType) {
         const newTicketsNum = ticketType === 'add' ? this.numTickets + 1 : this.numTickets - 1;
         this.numTickets = newTicketsNum;
     }
